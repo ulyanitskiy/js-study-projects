8a2a649e3f4624e89f00a7c9badd0169cf146133
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-07-31 21:23:28 +0300

    HW2 completed

---
+++ tasks/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Task #1 */
+let name = "John";
+let admin = name;
+alert(admin);
+
+/* Task #2 */
+let planetName = "Earth";
+let visitorName = "Alex";
+
+/* Task #3 */
+const sum = '10' + 10;
+console.log(typeof sum);
