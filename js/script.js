8193c8e9fe3b0f211589870219ebd48ecfd423c9
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-07-31 21:58:02 +0300

    HW3 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const operandA = Number(prompt('Enter operand A'))
+const operandB = Number(prompt('Enter operand B'))
+
+let add = operandA + operandB
+let sub = operandA - operandB
+let mul = operandA * operandB
+let div = operandA / operandB
+
+console.log(`Результат: ${operandA} + ${operandB} = ${add}`)
+console.log(`Результат: ${operandA} + ${operandB} = ${sub}`)
+console.log(`Результат: ${operandA} + ${operandB} = ${mul}`)
+console.log(`Результат: ${operandA} + ${operandB} = ${div}`)
