04630f50e395aa7129c7d00f86adb175cab79142
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-08-01 15:21:02 +0300

    HW3 updated

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
 const operandA = Number(prompt('Enter operand A'))
 const operandB = Number(prompt('Enter operand B'))
 
-let add = operandA + operandB
-let sub = operandA - operandB
-let mul = operandA * operandB
-let div = operandA / operandB
+const add = operandA + operandB
+const sub = operandA - operandB
+const mul = operandA * operandB
+const div = operandA / operandB
 
 console.log(`Результат: ${operandA} + ${operandB} = ${add}`)
 console.log(`Результат: ${operandA} + ${operandB} = ${sub}`)
