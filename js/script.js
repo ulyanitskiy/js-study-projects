df68717b1cf5532044ea6c50b8acadd8c85b0d0b
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-08-06 15:43:43 +0300

    HW6 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// 1
+const name = prompt('What is your name?');
+const age = prompt('What is your age?');
+const likesProgramming = confirm(`${name}, do you like programming?`);
+
+// 2
+console.log(name);
+console.log(age);
+console.log(likesProgramming);
+console.log(typeof name, typeof age, typeof likesProgramming);
+
+// 3
+const greeting = `Hello, ${name}!`;
+alert(greeting);
+
+// 4
+const numericAge = Number(age);
+const nextAge = numericAge + 1;
+
+console.log(nextAge);
+
+// 5
+let isAdult;
+
+if (age >= 18) {
+    isAdult = true;
+} else {
+    isAdult = false;
+}
+
+console.log(isAdult);
+
+
