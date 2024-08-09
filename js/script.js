5c489fe2616e015f1a4f96dd2453493e3bc9a626
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-08-09 16:57:51 +0300

    HW8 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const operandA = Number(prompt('Введите операнд A'));
+const operandB = Number(prompt('Введите операнд B'));
+const operandC = Number(prompt('Введите операнд C'));
+const result = (operandA + operandB + operandC) / 3;
+
+alert(`Среднее арифметическое трёх операндов = ${result}`);
